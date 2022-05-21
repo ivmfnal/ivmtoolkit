073282f0de639c6ccd32a0bbf3e2ff77ddeadd8b
Igor Mandrichenko <ivm@fnal.gov>
2022-05-21 10:01:59 -0400

    .

--- cli/cli.py
+++ cli/cli.py
@@ -12,26 +12,37 @@ class UnknownCommand(Exception):
 class EmptyCommandLine(Exception):
     pass
     
-class CLICommand(object):
+class InvalidArguments(Exception):
+    pass
+    
+class InvalidOptions(Exception):
+    pass
+    
+class CLIInterpreter(object):
 
-    Opts = ""
+    Opts = ("", [])
     Usage = ""
     Defaults = {}
+    MinArgs = 0
 
-    def run(self, command, argv, *params, **kwargs):
-        short_opts = ""
-        long_opts = []
-        try:
-            opts = self.Opts
-        except AttributeError:
-            pass
-        if isinstance(opts, tuple):
-            short_opts, long_opts = opts
-        elif isinstance(opts, str):
-            short_opts = opts
-        elif isinstance(opts, list):
-            long_opts = opts
-        opts, args = getopt.getopt(argv, short_opts, long_opts)
+    def get_options(self):
+        tup = self.Opts
+        out = ("", [])
+        if isinstance(tup, str):
+            words = tup.split()
+            if words:
+                if words[0] == "--":
+                    out = ("", words[1:])
+                else:
+                    out = (words[0], words[1:])
+        elif isinstance(tup, list):
+            out = ("", tup)
+        else:
+            assert isinstance(tup, tuple) and len(tup) == 2
+            out = tup
+        return out
+
+    def make_opts_dict(self, opts):
         opts_dict = {}
         for opt, val in opts:
             existing = opts_dict.get(opt)
@@ -41,32 +52,45 @@ class CLICommand(object):
                 existing.append(val)
             else:
                 opts_dict[opt] = [existing, val]
-        out_opts = self.Defaults.copy()
-        out_opts.update(opts_dict)
-        return self(command, out_opts, args, *params, **kwargs)
+        out = self.Defaults.copy()
+        out.update(opts_dict)
+        return out
 
-    @classmethod
-    def usage(cls, first_indent="", rest_indent=None):
-        if rest_indent is None:
-            rest_indent = first_indent
+    def getopt(self, argv):
+        short_opts, long_opts = self.get_options()
         try:
-            usage = cls.Usage
-        except AttributeError:
-            usage = ""
-        if "\n" in usage:
-            first_line, rest = usage.split("\n", 1)
-            first_line = first_line.strip()
-            rest = textwrap.dedent(rest)
-            return first_indent + first_line + "\n" + textwrap.indent(rest, rest_indent)
-        else:
-            return usage.strip()
+            opts, args = getopt.getopt(argv, short_opts, long_opts)
+        except getopt.GetoptError:
+            raise InvalidOptions()
+        if len(args) < self.MinArgs:
+            raise InvalidArguments()
+        return self.make_opts_dict(opts), args
+    
+    # overridable
+    def _run(self, command, context, argv, usage_on_empty = True, usage_on_unknown = True):
+        return None
         
-    @classmethod
-    def format_usage(cls, first_indent, rest_indent):
+    
+class CLICommand(CLIInterpreter):
+
+    def _run(self, command, context, argv, usage_on_error = True):
         try:
-            usage = cls.Usage
+            opts, args = self.getopt(argv)
+        except (InvalidOptions, InvalidArguments):
+            if usage_on_error:
+                cmd = "" if not command else f"for {command}"
+                print(f"Invalid arguments or options for {cmd}\n", file=sys.stderr)
+                print(self.usage(), file=sys.stderr)
+            else:
+                raise
+        return self(command, context, opts, args)
+
+    def format_usage(self, first_indent, rest_indent):
+        try:
+            usage = self.Usage
         except AttributeError:
             usage = ""
+        usage = usage.rstrip()
         if "\n" in usage:
             first_line, rest = usage.split("\n", 1)
             rest = rest.rstrip()
@@ -75,15 +99,15 @@ class CLICommand(object):
         else:
             return [first_indent + usage.strip()]
         
-    @classmethod
-    def usage(cls, first_indent="", rest_indent=None):
+    def usage(self, first_indent="", rest_indent=None):
         if rest_indent is None:
             rest_indent = first_indent
-        return "\n".join(cls.format_usage(first_indent, rest_indent))
+        return "\n".join(self.format_usage(first_indent, rest_indent))
 
-class CLI(object):
+class CLI(CLIInterpreter):
     
-    def __init__(self, *args):
+    def __init__(self, *args, hidden=False):
+        self.Hidden = hidden
         groups = []
         group = []
         group_name = ""
@@ -110,21 +134,11 @@ class CLI(object):
             raise ValueError("Only first group can be unnamed")
         self.Groups.append((title, commands))
         
-    def run(self, argv, *params, usage_on_empty = True, usage_on_unknown = True, **kwargs):
-        
-        if not argv:
-            if usage_on_empty:
-                print(self.usage(), file=sys.stderr)
-                return
-            else:
-                raise EmptyCommandLine()
-        
-        word, rest = argv[0], argv[1:]
-        
-        if word in ("-h", "-?", "--help"):
-            self.print_usage()
-            return
-
+    # overridable
+    def update_context(self, context, opts, args):
+        return context
+    
+    def find_interpreter(self, word):
         interp = None
         for group_name, commands in self.Groups:
             for w, c in commands:
@@ -133,48 +147,97 @@ class CLI(object):
                     break
             if interp is not None:
                 break
-        else:
-            if usage_on_unknown:
+        #print(self.__class__.__name__, f".find_interpreter({word}) ->", interp)
+        return interp
+
+    def _run(self, command, context, argv, usage_on_error = True):
+        try:
+            opts, args = self.getopt(argv)
+        except (InvalidOptions, InvalidArguments):
+            if usage_on_error:
+                cmd = "" if not command else f"for {command}"
+                print(f"Invalid arguments or options for {cmd}\n", file=sys.stderr)
                 print(self.usage(), file=sys.stderr)
+                return
+            else:
+                raise
+
+        if not args:
+            if usage_on_error:
+                print(self.usage(), file=sys.stderr)
+                return
+            else:
+                raise EmptyCommandLine()
+            
+
+        #print(f"{self.__class__.__name__}._run(): argv:", argv, "  args:", args)
+
+        context = self.update_context(context, opts, args)
+        word, rest = args[0], args[1:]
+        
+        if word in ("help", "-h", "-?", "--help"):
+            print(self.usage(long=True), file=sys.stderr())
+        
+        interp = self.find_interpreter(word)
+        if interp is None:
+            print(f"Unknown command {command} {word}\n", file=sys.stderr)
+            if usage_on_error:
+                indent = "" if not command else "  "
+                print("Usage:" if not command else f"Usage for {command}:\n", 
+                      self.usage(indent=indent),
+                      file=sys.stderr)
                 return
             else:
                 raise UnknownCommand(word, argv)
         
-        if isinstance(interp, CLI):
-            return interp.run(rest, *params, 
-                        usage_on_empty = usage_on_empty, usage_on_unknown = usage_on_unknown,
-                        **kwargs)
-        else:
-            return interp().run(word, rest, *params, **kwargs)
+        return interp._run(word, context, rest, usage_on_error = usage_on_error)
+        
+    def run(self, argv, context=None, usage_on_error = True):
+        self._run("", context, argv, usage_on_error)
 
-    def usage(self, as_list=False, headline="Usage:", end="", indent=""):
+    def usage(self, headline="", as_list=False, long=True, end="", indent=""):
+
+        if self.Hidden:
+            return [] if as_list else ""
+        
         out = []
+        
         if headline:
             out.append(indent + headline)
 
         maxcmd = 0
+        maxgroup = 0
         for group_name, interpreters in self.Groups:
             maxcmd = max(maxcmd, max(len(w) for (w, _) in interpreters))
-        
-        for i, (group_name, interpreters) in enumerate(self.Groups):
-            if group_name:
-                out.append(indent + group_name)
-            fmt = f"%-{maxcmd}s %s"
-            offset = "  " if group_name else ""
-            for i, (word, interp) in enumerate(interpreters):
-                #if i > 0:
-                #    out.append("")
-                if isinstance(interp, CLI):
-                    out.append(indent + offset + word)
-                    usage = interp.usage(headline="", indent=indent + offset + "  ")
-                    out.append(usage)
-                else:
-                    # assume CLICommand subclass
-                    #usage = interp.usage(" "*(maxcmd-len(word)), indent + " "*(maxcmd+1))
-                    #usage = interp.usage("", indent + " "*(maxcmd+1))
-                    usage = interp.usage("", indent + "  ")
-                    out.append(indent + word + " " + usage)
-                    out.append("")
+            maxgroup = max(len(group_name), maxgroup)
+
+        if not long:
+            for group_name, interpreters in self.Groups:
+                head = (f"%-{maxgroup}s: " % (group_name,)) if group_name else ""
+            line = head + ",".join(w for w, i in interpreters)
+            out.append(indent + line)
+        else:
+            for i, (group_name, interpreters) in enumerate(self.Groups):
+                if group_name:
+                    out.append(indent + group_name)
+                fmt = f"%-{maxcmd}s %s"
+                offset = "  " if group_name else ""
+                for i, (word, interp) in enumerate(interpreters):
+                    if isinstance(interp, CLI):
+                        if not interp.Hidden:
+                            out.append(indent + offset + word)
+                            usage = interp.usage(headline="", indent=indent + offset + "    ", long=False)
+                            out.append(usage)
+                    elif isinstance(interp, CLICommand):
+                        # assume CLICommand subclass
+                        #usage = interp.usage(" "*(maxcmd-len(word)), indent + " "*(maxcmd+1))
+                        #usage = interp.usage("", indent + " "*(maxcmd+1))
+                        usage = interp.usage("", indent + "    ")
+                        pre_word = "" if usage.strip().split(None, 1)[0] == word else word + " "
+                        out.append(indent + pre_word + usage)
+                        out.append("")
+                    else:
+                        raise ValueError("Unrecognized type of the interpreter: %s %s" % (type(interp), interp))
         #print(self, f": usage:{out}")
         if as_list:
             return out
