d2fe8fefde7ff3fe09ff06a87936ef8fb03b46b1
Igor Mandrichenko <ivm@fnal.gov>
2022-08-09 14:13:18 -0400

    Made Tracer recursive

--- trace.py
+++ trace.py
@@ -1,10 +1,69 @@
 import time
 
-class TracePoint(object):
-    def __init__(self, name):
+class Tracer(object):
+
+    def __init__(self, path="", tzero=0.0, calibrate=False):
+        self.Path = path
+        self.Points = {}
+        self.TZero = tzero
+        if calibrate:   self.calibrate()
+
+    def __getitem__(self, name):
+        point = self.Points.get(name)
+        if point is None:
+            path = name if not self.Path else self.Path + '/' + name
+            point = self.Points[name] = TracePoint(name, path, self.TZero)
+        return point
+
+    def stats(self):
+        out = []
+        for n, p in self.Points.items():
+            out += p.stats()
+        return sorted(out)
+
+    def format(self, as_list=False):
+        stats = self.stats()
+        maxp = 4
+        for t in stats:
+            path = t[0]
+            maxp = max(len(path), maxp)
+        headfmt = f"%-{maxp}s %8s %8s %8s"
+        datafmt = f"%-{maxp}s %8d %8.3f %8.3f"
+        div = "-"*maxp + " -------- -------- --------"
+        out = [
+            headfmt % ("Point", "Count", "Total", "Average"),
+            div
+        ] + [datafmt % tup for tup in self.stats()] + [div]
+        if as_list:
+            return out
+        else:
+            return "\n".join(out)
+        
+    def reset(self):
+        self.Points = {}     
+        
+    def calibrate(self):
+        # not really working
+        t = Tracer()
+        N = 100
+        d = 0.001
+        tx = t["x"]
+        ty = tx["y"]
+        n = 0
+        for _ in range(N):   
+            with tx:
+                with ty:
+                    n += 1
+        self.TZero = (tx.Time - ty.Time)/N
+        return n
+    
+class TracePoint(Tracer):
+    def __init__(self, name, path, tzero):
+        Tracer.__init__(self, path, tzero)
         self.Name = name
-        self.T0 = None
+        self.TZero = tzero
         self.reset()
+        self.T0 = time.time()
         
     def reset(self):
         self.Count = 0
@@ -19,72 +78,15 @@ class TracePoint(object):
         self.Time += time.time() - self.T0
         return self
         
-    def stats(self, t0 = 0.0):
+    def stats(self):
         avg = None
-        if self.Count > 0:  avg = self.Time/self.Count - t0
-        return self.Count, self.Time - t0*self.Count, avg
+        if self.Count > 0:  avg = self.Time/self.Count - self.TZero
+        return [(self.Path, self.Count, self.Time - self.TZero*self.Count, avg)] + Tracer.stats(self)
 
     def __enter__(self):
         self.begin()
+        return self
 
     def __exit__(self, et, ev, tb):
         self.end()
 
-class Tracer(object):
-
-    def __init__(self, calibrate = False):
-        self.Points = {}
-        self.TZero = 0.0
-        if calibrate:   self.calibrate()
-        
-    def __getitem__(self, name):
-        point = self.Points.get(name)
-        if point is None:
-            point = self.Points[name] = TracePoint(name)
-        return point
-        
-    def begin(self, name):
-        return self[name].begin()
-        
-    def end(self, name):
-        return self[name].end()
-    
-    def stats(self):
-        return [(n, p.stats(self.TZero)) for n, p in self.Points.items()]
-        
-    def formatStats(self):
-        lst = self.stats()
-        lst.sort()
-        out = []
-        for name, (count, total, average) in lst:
-            out.append("%-40s: %-6d %f %f" % (name, count, total, average))
-        return "\n".join(out)
-        
-    def printStats(self):
-        print(self.formatStats())
-        
-    def reset(self):
-        self.Points = {}     
-        
-    def calibrate(self):
-        t = Tracer()
-        for _ in range(10000):   
-            with t["x"]:
-                pass
-        tx = t["x"]
-        self.TZero = tx.Time/tx.Count
-        
-if __name__ == '__main__':
-    T = Tracer()
-    for i in range(100):
-        T.op1.begin()
-        time.sleep(0.1)
-        T.op1.end()
-        
-        if i % 3:
-            T.op2.begin()
-            time.sleep(0.2)
-            T.op2.end()
-        if (i%10) == 0:
-            for n, st in T.stats():
-                print('%s: %s %s %s' % ((n,)+st))
